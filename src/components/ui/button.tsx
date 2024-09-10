962468bcba3a21c5773ae992dfb0961e53fb4ede
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2024-09-10 23:20:57 +0700

    fix: build

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,57 +1,62 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from 'react';
 
-import { cn } from "@/lib/utils"
+import { Slot } from '@radix-ui/react-slot';
+import { cn, withRef } from '@udecode/cn';
+import { type VariantProps, cva } from 'class-variance-authority';
 
-const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+export const buttonVariants = cva(
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
+    defaultVariants: {
+      size: 'default',
+      variant: 'default',
+    },
     variants: {
-      variant: {
-        default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+      isMenu: {
+        true: 'h-auto w-full cursor-pointer justify-start',
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        default: 'h-10 px-4 py-2',
+        icon: 'size-10',
+        lg: 'h-11 rounded-md px-8',
+        none: '',
+        sm: 'h-9 rounded-md px-3',
+        sms: 'size-9 rounded-md px-0',
+        xs: 'h-8 rounded-md px-3',
+      },
+      variant: {
+        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        destructive:
+          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        inlineLink: 'text-base text-primary underline underline-offset-4',
+        link: 'text-primary underline-offset-4 hover:underline',
+        outline:
+          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+        secondary:
+          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
       },
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
-    },
   }
-)
+);
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean
-}
+export type ButtonProps = {
+  asChild?: boolean;
+} & VariantProps<typeof buttonVariants>;
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Button.displayName = "Button"
+export const Button = withRef<
+  'button',
+  {
+    asChild?: boolean;
+  } & VariantProps<typeof buttonVariants>
+>(({ asChild = false, className, isMenu, size, variant, ...props }, ref) => {
+  const Comp = asChild ? Slot : 'button';
 
-export { Button, buttonVariants }
+  return (
+    <Comp
+      className={cn(buttonVariants({ className, isMenu, size, variant }))}
+      ref={ref}
+      {...props}
+    />
+  );
+});
